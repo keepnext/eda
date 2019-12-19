9ea805835d97a86f8265c24caa7a32954ada8a30
keepnext <keepnext@gmail.com>
2019-12-19 15:21:49 +0900

    add file

---
+++ ch01/1.opBasic.py
@@ -0,0 +1,34 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Dec 19 14:00:06 2019
+
+@author: edu
+"""
+
+a = 10
+#%%
+b = 20.0
+c = a + b
+#%%
+print(a)
+#%%
+print(b)
+#%%
+print(c)
+
+#%%
+age = 17
+h = 180.4
+
+print("Input name:")
+name = input();
+print("your name is" + name)
+print("Input name is %s, %i, %f" % (name, age, h))
+
+
+#%%
+name = input('Input name:')
+print('your name is ' + name)
+
+
+
